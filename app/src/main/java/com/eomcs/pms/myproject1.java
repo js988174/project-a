cc0a94428be201d7cdc7c6441bb8810a2a7a2dec
js988174 <rudtjs4902@naver.com>
2021-01-18 18:33:53 +0900

    두번째 백업

--- app/src/main/java/com/eomcs/pms/myproject1.java
+++ app/src/main/java/com/eomcs/pms/myproject1.java
@@ -19,160 +19,167 @@ public class myproject1 {
  번호 중복없이 랜덤지정 해주기 
    * 
    */
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
+  
+   static Scanner scanner = new Scanner(System.in);
+   static Random random = new Random();
     // 회원 등록 데이터
-    final int MEMBER = 1000;
-    String[] id = new String[MEMBER];
-    int[] number = new int[1];
-    String[] name = new String[MEMBER];
-    String[] phone = new String[MEMBER];
-    String[] adress = new String[MEMBER];
-    String[] birth = new String[MEMBER];
-    String[] job = new String[MEMBER];
-    String[] gender = new String[MEMBER];
-    String[] email = new String[MEMBER];
-    Date[] now = new Date[MEMBER];
-    int[] status = new int[MEMBER];
+   static final int MEMBER = 1000;
+   static String[] id = new String[MEMBER];
+   static int[] number = new int[1];
+   static String[] name = new String[MEMBER];
+   static String[] phone = new String[MEMBER];
+   static String[] adress = new String[MEMBER];
+   static String[] birth = new String[MEMBER];
+   static String[] job = new String[MEMBER];
+   static String[] gender = new String[MEMBER];
+   static String[] email = new String[MEMBER];
+   static Date[] now = new Date[MEMBER];
+   static int[] status = new int[MEMBER];
  
     
-    int size = 0;
-    // 기존 회원 데이터
-  
+   static int size = 0;
    
+   public static void main(String[] args) {
    
-
+loop:
     while(true) {
       System.out.println("1. 신규 회원 2. 기존 회원 3. 관리자 모드 4. 종료 ");
       System.out.print("번호를 선택해주세요");
       System.out.print("> ");
-      int command = scanner.nextInt();
+      String command = scanner.nextLine();
     
 
-      if(command == 1) {
-        for (int i =  0; i < MEMBER; i++) {
-          System.out.println();
-          System.out.println("[회원 등록]");
-          
-          System.out.println("회원 번호 지정: ");
-          System.out.printf("> ");
-          for (int j = 0; j < number.length; j++) {
-            number[j] = random.nextInt(1000)+1;
-           System.out.println(number[j]);
-          }
-          
-         System.out.println();
-         scanner.nextLine();
-          System.out.println("id: ");
-          System.out.printf("> ");
-          id[i]= scanner.nextLine();
-          
-          System.out.println("회원 이름: ");
-          System.out.printf("> ");
-          name[i] = scanner.nextLine();
-
-          System.out.println("전호 번호: ");
-          System.out.printf("> ");
-          phone[i] = scanner.nextLine();
-
-          System.out.println("주소: ");
-          System.out.printf("> ");
-          adress[i] = scanner.nextLine();
-
-          System.out.println("생년 월일: ");
-          System.out.printf("> ");
-          birth[i] = scanner.nextLine();
-
-          System.out.println("직업: ");
-          System.out.printf("> ");
-          job[i] = scanner.nextLine();
-
-          System.out.println("성별: ");
-          System.out.printf("> ");
-          gender[i] = scanner.nextLine();
-
-          System.out.println("이메일: ");
-          System.out.printf("> ");
-          email[i] = scanner.nextLine();
-          
-               
-          now[i] = new java.sql.Date(System.currentTimeMillis());
-          
-
-          System.out.println("등록 기간: ");
-          System.out.println("0: 3개월 ");
-          System.out.println("1: 6개월 ");
-          System.out.println("2: 12개월 ");
-          status[i] = Integer.valueOf(scanner.nextLine());
-
-          size++;
-          break;
-        }
-      }else if(command == 2) {
-        System.out.println("[기존 회원]");
-        System.out.println("회원 번호 입력: ");
-        System.out.printf("> "); 
-        System.out.println();
-        
-        System.out.println("남은 기간: ");
-        System.out.printf("> ");
-        System.out.println();
-        
-       System.out.println("bmi지수를 계산하시겠습니까? [yes/no] ");
-       scanner.nextLine();
-       String bmicalculation = scanner.nextLine();
-       if(bmicalculation.equals("yes")) {
-         
-        System.out.println("[bmi지수 계산기] ");
-        System.out.println("몸무게: ");
-        System.out.printf("> ");
-        double weight = scanner.nextDouble();
-
-        System.out.println("키: ");
-        System.out.printf("> ");
-        double height = scanner.nextDouble();
-
-        double bmi = weight/height/height ;
-        System.out.printf("bmi 지수: %.2f\n" ,bmi);
-       }else {
-         System.out.println("메뉴로 돌아갑니다.");
-       }
-      }else if (command ==3) {
-        // 회원 목록과 남은 일수 
-        System.out.println("[관리자 모드]");
-        for (int i = 0; i < size; i++) {
-          String status1 = null;
-          switch (status[i]) {
-            case 1:
-              status1 = "3개월 신청";
-              break;
-            case 2:
-              status1 = "6개월 신청";
-              break;
-            default :
-              status1 = "12개월 신청";
-              break;
-          }
-          System.out.printf("회원 id: %s ,회원 번호: %d ,회원 이름: %s ,전화 번호: %s ,"
-              + " 주소: %s ,생년 월일: %s  ,직업: %s ,성별: %s ,이메일: %s ,등록 날짜: %s ,등록 기간: %s\n"
-              ,   id[i], number[i] , name[i], phone[i], adress[i], birth[i] , job[i], gender[i] 
-                  ,email[i], now[i], status1 );
-
-        }
-      }else if (command == 4) {
-        System.out.println("시스템을 종료합니다. ");
+      switch (command) {
+        case "1" :   
+          newMember();
+      break;
+      case "2" :  
+        existingMember();
+       break;
+      case "3" :
+        adminMode();
         break;
+      case "4" :
+        System.out.println("시스템을 종료합니다. ");
+        break loop;
 
-      }else {
+     default :
+        System.out.println();
         System.out.println("잘못 입력하셨습니다. ");
         System.out.println("다시 입력해주세요. ");
-
-      }
-    
+        System.out.println();
     }
+  System.out.println();
+  }
+
     scanner.close();
   }
+
+static void newMember() {
+  System.out.println();
+  System.out.println("[회원 등록]");
+  
+  System.out.println("회원 번호 지정: ");
+  System.out.printf("> ");
+  for (int j = 0; j < number.length; j++) {
+    number[j] = random.nextInt(1000)+1;
+   System.out.println(number[j]);
+  }
+  
+ System.out.println();
+  System.out.println("id: ");
+  System.out.printf("> ");
+  id[size]= scanner.nextLine();
+  
+  System.out.println("회원 이름: ");
+  System.out.printf("> ");
+  name[size] = scanner.nextLine();
+
+  System.out.println("전호 번호: ");
+  System.out.printf("> ");
+  phone[size] = scanner.nextLine();
+
+  System.out.println("주소: ");
+  System.out.printf("> ");
+  adress[size] = scanner.nextLine();
+
+  System.out.println("생년 월일: ");
+  System.out.printf("> ");
+  birth[size] = scanner.nextLine();
+
+  System.out.println("직업: ");
+  System.out.printf("> ");
+  job[size] = scanner.nextLine();
+
+  System.out.println("성별: ");
+  System.out.printf("> ");
+  gender[size] = scanner.nextLine();
+
+  System.out.println("이메일: ");
+  System.out.printf("> ");
+  email[size] = scanner.nextLine();
+  
+       
+  now[size] = new java.sql.Date(System.currentTimeMillis());
+  
+
+  System.out.println("등록 기간: ");
+  System.out.println("0: 3개월 ");
+  System.out.println("1: 6개월 ");
+  System.out.println("2: 12개월 ");
+  status[size] = Integer.valueOf(scanner.nextLine());
+
+  size++;
+  }
+static void existingMember() {
+  System.out.println("[기존 회원]");
+  System.out.println("회원 번호 입력: ");
+  System.out.printf("> "); 
+  System.out.println();
+  
+  System.out.println("남은 기간: ");
+  System.out.printf("> ");
+  System.out.println();
+  
+ System.out.println("bmi지수를 계산하시겠습니까? [yes/no] ");
+ String bmicalculation = scanner.nextLine();
+ if(bmicalculation.equalsIgnoreCase("yes")) {
+   
+  System.out.println("[bmi지수 계산기] ");
+  System.out.println("몸무게: ");
+  System.out.printf("> ");
+  double weight = scanner.nextDouble();
+
+  System.out.println("키: ");
+  System.out.printf("> ");
+  double height = scanner.nextDouble();
+
+  double bmi = weight/height/height ;
+  System.out.printf("bmi 지수: %.2f\n" ,bmi);
+ }else {
+   System.out.println("메뉴로 돌아갑니다.");
+ }
+  }
+static void adminMode() {
+  // 회원 목록과 남은 일수 
+  System.out.println("[관리자 모드]");
+  for (int i = 0; i < size; i++) {
+    String status1 = null;
+    switch (status[i]) {
+      case 1:
+        status1 = "3개월 신청";
+        break;
+      case 2:
+        status1 = "6개월 신청";
+        break;
+      default :
+        status1 = "12개월 신청";
+       
+    }
+    System.out.printf("회원 id: %s ,회원 번호: %d ,회원 이름: %s ,전화 번호: %s ,"
+        + " 주소: %s ,생년 월일: %s  ,직업: %s ,성별: %s ,이메일: %s ,등록 날짜: %s ,등록 기간: %s\n"
+        ,   id[i], number[i], name[i], phone[i], adress[i], birth[i] , job[i], gender[i] 
+            ,email[i], now[i], status1 );
+    }
 }
-
-
+}
